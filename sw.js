e11a6db757529c1ffc484070a4b54f2d6794c685
evadribina14-hash <eva.dribina14@gmail.com>
2026-09-24 14:22:13 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE = 'mans-budzets-soft-blue-v9';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest?v=9',
+  './icon-192.png?v=9',
+  './icon-512.png?v=9',
+  './favicon.png?v=9'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (
+    event.request.mode === 'navigate' ||
+    url.pathname.endsWith('/index.html')
+  ) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put('./index.html', copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
